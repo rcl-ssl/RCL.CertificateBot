527da04d3a219df9de559f455105798549e999d9
Ray Consulting <rayconsulting@shopeneur.com>
2022-05-23 15:08:32 -0400

    updated IIS Worker Service

--- RCL.CertificateBot.IIS/Program.cs
+++ RCL.CertificateBot.IIS/Program.cs
@@ -1,8 +1,15 @@
 using RCL.CertificateBot.IIS;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .UseWindowsService()
+    .ConfigureLogging((_, logging) => logging.AddEventLog())
+    .ConfigureServices((hostContext, services) =>
     {
+        IConfiguration Configuration = hostContext.Configuration;
+
+        services.AddRCLSDKService(options => Configuration.Bind("RCLSDK", options));
+        services.AddCertificateBotService(options => Configuration.Bind("CertificateBot", options));
+
         services.AddHostedService<Worker>();
     })
     .Build();
